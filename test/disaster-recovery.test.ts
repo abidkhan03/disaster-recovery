cccaead571c80abfb5fec216f840e620aaa211a5
abidkhan03 <abidkhan42935@gmail.com>
2024-02-15 17:33:26 +0500

    Initial commit

---
+++ test/disaster-recovery.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DisasterRecovery from '../lib/disaster-recovery-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/disaster-recovery-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DisasterRecovery.DisasterRecoveryStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
